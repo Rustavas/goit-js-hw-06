a2ca0bd58f240d3a805c3750e7b318e38dfcfef1
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-03-23 22:47:26 +0200

    Complited

--- js/task-06.js
+++ js/task-06.js
@@ -1,46 +1,18 @@
 
 
-const inputText = document.getElementById('validation-input');
-const dataLength = Number(inputText.dataset.length);
+const inputTextRef = document.getElementById('validation-input');
+const dataLength = Number(inputTextRef.dataset.length);
 
 
+inputTextRef.addEventListener('blur', onCheckNumber)
 
+function onCheckNumber (event) {
+if(event.currentTarget.value.length === dataLength){
+  inputTextRef.classList.remove('invalid');
+  inputTextRef.classList.add('valid');
+  return;
+} 
+  inputTextRef.classList.remove('valid');
+  inputTextRef.classList.add('invalid');
 
-function qwe (event) {
-  event.currentTarget
-  console.dir(event.currentTarget.value);
-
-
-}
-
-// inputText.addEventListener('blur', onInputTextBlur);
-
-// function onInputTextBlur(event) {
-
-
-  // if (
-  //   event.currentTarget.text.value.length === dataLength)
-  //  {
-
-  // inputText.classList.add("valid");
-
-  // } else {
-  // inputText.classList.remove("valid");
-  // inputText.classList.add("invalid");
-  // }
-  // }
-//!============
-  // const inputEl = document.querySelector('input');
-
-// inputEl.addEventListener('blur', onInputBlur);
-
-// function onInputBlur(event) {
-
-//     const total = event.currentTarget.value.length
-
-//     if (Number(inputEl.dataset.length) !== total) {
-//         inputEl.classList.add('invalid');
-//         return;
-//     }
-//     inputEl.classList.add('valid');
-// }
+};
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtnRef = document.querySelector('.change-color');
+const spanColorRef = document.querySelector('.change-color');
+
+changeColorBtnRef.addEventListener('click', onChangeColor);
+
+
+function onChangeColor(){
+  document.body.style.backgroundColor = getRandomHexColor();
+  console.clear();
+  console.log(spanColorRef.textContent = getRandomHexColor());
+};
